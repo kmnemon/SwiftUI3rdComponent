6828024ee99bda21db225510ceae130e68807dd5
kmnemon <kmnemon@outlook.com>
2024-06-02 12:10:53 +0800

    WebView

---
+++ SwiftUI3rdComponent/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  SwiftUI3rdComponent
+//
+//  Created by ke on 2024/6/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            NavigationStack{
+                List{
+                    NavigationLink("WebViewComponent"){
+                        WebViewComponent()
+                    }
+                }
+                .navigationTitle("Choose Component")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI3rdComponent/SwiftUI3rdComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3rdComponentApp.swift
+//  SwiftUI3rdComponent
+//
+//  Created by ke on 2024/6/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3rdComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
